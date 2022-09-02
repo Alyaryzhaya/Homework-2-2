6890ef9cc9f600cce6ff6e50f4f83058ca969374
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-02 18:50:17 +0300

    Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        int balance = 1000; // Количество денег на текущем счёте (переменная)
+        int refill = 1150; // сумма пополнения (переменная)
+        int bonuses = (refill- 1000)/100; // количество бонусных рублей (результат)
+        int total = balance + refill + bonuses; // итоговый счёт (результат)
+
+
+        if (refill > 1000) {
+            System.out.println("Количество бонусных рублей: " + bonuses );
+            System.out.println("Итого на счете: " + total );
+        } else{
+            System.out.println("Количество бонусных рублей: " + 0);
+            System.out.println("Итого на счете: " + (balance + refill));
+        }
+
+
+    }
 }
+
+
+
